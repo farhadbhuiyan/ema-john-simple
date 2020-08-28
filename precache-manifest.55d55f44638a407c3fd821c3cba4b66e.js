b0dc4ebbdae68bd9f7d2ac45ed8e7ed7fef3df84
farhadbhuiyan <farhadbhuiyan.cse@gmail.com>
2020-08-28 21:59:23 +0600

    Updates

---
+++ precache-manifest.55d55f44638a407c3fd821c3cba4b66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67aaea6efdbb789b59dc24602917488",
+    "url": "/ema-john-simple/index.html"
+  },
+  {
+    "revision": "ee9b398d365b463fc8a3",
+    "url": "/ema-john-simple/static/css/main.80f75778.chunk.css"
+  },
+  {
+    "revision": "e0cdb01ef919e64a3fdb",
+    "url": "/ema-john-simple/static/js/2.045a1b7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ema-john-simple/static/js/2.045a1b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9b398d365b463fc8a3",
+    "url": "/ema-john-simple/static/js/main.0d36b697.chunk.js"
+  },
+  {
+    "revision": "9b71f17624f0a3268932",
+    "url": "/ema-john-simple/static/js/runtime-main.bde08057.js"
+  },
+  {
+    "revision": "c9fedba82e11c07c82b90cae8983442d",
+    "url": "/ema-john-simple/static/media/logo1.c9fedba8.png"
+  }
+]);
